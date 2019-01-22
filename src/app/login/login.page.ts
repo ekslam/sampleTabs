7f3f5cc403753d2859b8f3e3147e6de77979734a
Kevin <edwardkevinl1@gmail.com>
2019-01-22 21:11:58 +0800

    New template

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { RouterModule, Routes, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  
+
+
+  ngOnInit() {
+  }
+
+  async navTabs(){
+    this.routes.navigateByUrl('/app/tabs/(home:home)');
+    
+  }
+  
+
+}
